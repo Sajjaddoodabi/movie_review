d8f456e04f0ffa8bfe02d163e6fbba1496d0ec89
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-11 01:11:28 +0330

    add register view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from .models import User
+from .serializers import UserSerializer
+
+
+class RegisterView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
