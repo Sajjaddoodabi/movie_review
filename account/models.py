616956849370c5daaf95c92c427982aba517c00b
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-10 15:30:55 +0330

    start user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    def __str__(self):
+        if self.get_full_name() != ' ':
+            return self.get_full_name()
+        return self.username
