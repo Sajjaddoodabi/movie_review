abc66991fab24e159a7ed703f14435646026e225
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-17 18:44:29 +0330

    add genre and movie detail view set

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
+from .serializers import MovieSerializer, GenreSerializer
+from .models import Movie, Genre
 
-# Create your views here.
+
+class GenreViewSet(viewsets.ModelViewSet):
+    serializer_class = GenreSerializer
+    queryset = Genre.objects.all()
+
+
+class CreateMovieView(APIView):
+    def post(self, request):
+        pass
+
+
+class MovieDetailView(mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin,
+                      mixins.ListModelMixin,
+                      GenericViewSet):
+    serializer_class = MovieSerializer
+    queryset = Movie.objects.all()
