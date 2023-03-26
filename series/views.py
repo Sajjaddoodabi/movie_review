0a74b60d909375831d58db4a09e0fb3add399bd6
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-26 20:01:04 +0430

    add serial comment view set

--- series/views.py
+++ series/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
-from .models import Series, Episode
-from .serializers import SerialSerializer, SerialMiniSerializer, EpisodeSerializer
+from rest_framework.decorators import action
+from rest_framework.views import APIView
+
+from .models import Series, Episode, SerialComment
+from .serializers import SerialSerializer, SerialMiniSerializer, EpisodeSerializer, SerialCommentSerializer
 
 
 class SerialViewSet(viewsets.ModelViewSet):
@@ -12,3 +15,11 @@ class EpisodeViewSet(viewsets.ModelViewSet):
     serializer_class = EpisodeSerializer
     queryset = Episode.objects.all()
 
+
+class SerialCommentViewSet(viewsets.ModelViewSet):
+    serializer_class = SerialCommentSerializer
+    queryset = SerialComment.objects.all()
+
+    @action(detail=True, methods=['POST'])
+    def approve_comment(self, request, pk):
+        pass
