6feea86723e951e79848f788a7e80c25e816a4a3
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-26 15:28:58 +0430

    modified admin panel for movies

--- movies/admin.py
+++ movies/admin.py
@@ -4,8 +4,21 @@ from .models import Genre, Movie, MovieComment
 
 @admin.register(Movie)
 class BaseAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ['year', 'is_active']
+    list_display = ['title', 'imdb_rate', 'year', 'is_active']
+    list_editable = ['is_active']
 
 
-admin.site.register(Genre)
-admin.site.register(MovieComment)
+@admin.register(Genre)
+class BaseAdmin(admin.ModelAdmin):
+    list_filter = ['is_active']
+    list_display = ['title', 'is_active']
+    list_editable = ['is_active']
+
+
+@admin.register(MovieComment)
+class BaseAdmin(admin.ModelAdmin):
+    list_filter = ['is_active', 'is_approve']
+    list_display = ['user', 'movie', 'is_active', 'is_approve']
+    list_editable = ['is_active', 'is_approve']
+
