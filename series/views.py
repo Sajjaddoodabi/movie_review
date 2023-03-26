b432e4cd1607e4c5ad76c6d58bf15ec0e8caad15
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-26 15:33:07 +0430

    add serial view set

--- series/views.py
+++ series/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Series, Episode
+from .serializers import SerialSerializer, SerialMiniSerializer, EpisodeSerializer
 
-# Create your views here.
+
+class SerialViewSet(viewsets.ModelViewSet):
+    serializer_class = SerialSerializer
+    queryset = Series.objects.all()
+
