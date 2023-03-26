92130d95c444a7486076ee7bbc991828ecbee9fb
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-26 15:33:56 +0430

    add episode view set

--- series/views.py
+++ series/views.py
@@ -7,3 +7,8 @@ class SerialViewSet(viewsets.ModelViewSet):
     serializer_class = SerialSerializer
     queryset = Series.objects.all()
 
+
+class EpisodeViewSet(viewsets.ModelViewSet):
+    serializer_class = EpisodeSerializer
+    queryset = Episode.objects.all()
+
