ca0de393bba40925782fa734907eeb7c2f9e05f7
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-30 12:55:53 +0430

    add cast and crew views

--- cast_and_crew/views.py
+++ cast_and_crew/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Director, Actor
+from .serializers import BaseCastSerializer
+
+
+class DirectorViewSet(viewsets.ModelViewSet):
+    serializer_class = BaseCastSerializer
+    queryset = Director
+
+
+class ActorViewSet(viewsets.ModelViewSet):
+    serializer_class = BaseCastSerializer
+    queryset = Actor
